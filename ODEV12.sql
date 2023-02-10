62acc6aeffc32f43ca3507b9424a023bf78a6acd
grebaran <116900035+grebaran@users.noreply.github.com>
2023-02-10 10:46:13 +0300

    Update ODEV12.sql

--- ODEV12.sql
+++ ODEV12.sql
@@ -7,5 +7,16 @@ SELECT COUNT(*) FROM film
 WHERE rental_rate=(SELECT MAX(rental_rate) FROM film);
 
 --film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT title FROM film
+WHERE rental_rate=(SELECT MIN(rental_rate) FROM film) 
+AND 
+replacement_cost=(SELECT MIN(replacement_cost) FROM film);
 
 --payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT first_name,last_name FROM customer
+WHERE customer_id=
+
+(SELECT customer_id FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(*) DESC
+LIMIT 1);
